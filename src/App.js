13864eb787b74551f585bf22b90f0924d39b7186
Caeyers Denis <denis.caeyers@vito.be>
2019-07-09 09:37:38 +0200

    Customer fiddling

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+
+// Router
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// Tabler-React
+import { Site,Nav,Page } from "tabler-react";
+import "tabler-react/dist/Tabler.css";
+
+// SCSS
+import './styles/App.scss';
+
+// Components
+import KlantLijst from './klanten/KlantLijst'
+
+function AppRouter() {
   return (
+
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Site>
+      <Router>
+        <Site.Header>
+        </Site.Header>
+        <Site.Nav>
+        <Nav>
+          <Nav.Item to="/klanten" icon="user">
+            Klanten
+          </Nav.Item>
+          <Nav.Item to="/bestelbonnen" icon="globe" >
+            Bestelbonnen
+          </Nav.Item>
+        </Nav>
+        </Site.Nav>
+        <Page>
+          <Page.Content>
+          <div id="main">
+            <Route path="/" exact component={Index} />
+            <Route path="/klanten/" component={Klanten} />
+            <Route path="/bestelbonnen/" component={Bestelbonnen} />
+          </div>
+          </Page.Content>
+        </Page>
+        
+      </Router>
+      </Site>
     </div>
   );
 }
 
-export default App;
+function Index() {
+  return <h2>Home</h2>;
+}
+
+function Klanten() {
+  return (
+    <div>
+    <h2>Klanten</h2>
+    <KlantLijst />
+    </div>
+  )
+}
+
+function Bestelbonnen() {
+  return <h2>Bestelbonnen</h2>;
+}
+
+
+
+export default AppRouter;
